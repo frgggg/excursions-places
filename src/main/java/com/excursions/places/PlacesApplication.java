3ce79fc7d8cc16363c447fe5a0276e1294f00395
Fedor <zhar.feda@yandex.ru>
2020-02-12 16:54:16 +0300

    places service funs: check ids functions in proc v6, no test

--- src/main/java/com/excursions/places/PlacesApplication.java
+++ src/main/java/com/excursions/places/PlacesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class ExcursionsPlacesApplication {
+public class PlacesApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ExcursionsPlacesApplication.class, args);
+        SpringApplication.run(PlacesApplication.class, args);
     }
 }
